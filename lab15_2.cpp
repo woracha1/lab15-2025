d7f7e027836b66e9b53712a765040fa71ed11a6d
GGTTGTGt <zz814170@gmail.com>
2025-01-28 18:02:24 +0700

    lab15_2 completed

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -2,12 +2,24 @@
 #include <algorithm>
 using namespace std;
 
+void sort(int x[]){
+    std :: sort(x,x+5) ;
+    std :: sort(x+5,x+10) ;
+}
+
 int main(){
-	int data[] = {7,8,2,1,2,4,6,9,0,1}; 
-	cout << "Before: ";
-	for(int i = 0; i<10; i++) cout << data[i] << " ";
-	sort(&data[0],&data[10]);
-	cout << "\nAfter: ";
-	for(int i = 0; i<10; i++) cout << data[i] << " ";
-	return 0;
+    int data[] = {7,8,2,1,2,4,6,9,0,1};
+    cout << "Before: " ;
+    for(int i = 0; i < 10 ; i++){
+        cout << data[i] << " " ;
+    }
+    cout << endl ;
+
+    sort(data);
+    cout << "After: " ;
+    for(int i = 0; i < 10 ; i++){
+        cout << data[i] << " " ;
+    }
+
+return 0 ;
 }
