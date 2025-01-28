9883370b423a2a04367d9bfeb0ddb5f1595cf26b
GGTTGTGt <zz814170@gmail.com>
2025-01-28 18:01:38 +0700

    lab15_1 completed

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,9 +1,27 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
 
-	
-	
-	return 0;
+    int a = 5 ;
+    string b = "A" ;
+    string &c = b ;
+    int *x ;
+    int **z ;
+    string *y ;
+    x = &a ;
+    y = &b ;
+    z = &x ;
+
+    cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << endl ;
+    cout << &a << " " << &b << " " << &c << " " << &x << " " << &y << " " << &z << endl ;
+    c = "F" ;
+    cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << endl ;
+    *y = "W" ;
+    cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << endl ;
+    *x = 6 ;
+    cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << endl ;
+    **z = 7 ;
+    cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << endl ;
+    return 0 ;
 }
